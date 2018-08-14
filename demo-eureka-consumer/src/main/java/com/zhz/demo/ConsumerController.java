803eafe23dde2e55c91301af5aa4532c2332e943
zhz <783633439@qq.com>
2018-08-14 19:14:17 +0800

    server-consumer

---
+++ demo-eureka-consumer/src/main/java/com/zhz/demo/ConsumerController.java
@@ -0,0 +1,33 @@
+package com.zhz.demo;/**
+ * Created by zhz on 18/8/14.
+ */
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 功能:
+ *
+ * @author zhz
+ * @date 18/8/14 上午1:45
+ */
+@RestController
+public class ConsumerController {
+    @Autowired
+    RestTemplate restTemplate;
+    @Autowired
+    LoadBalancerClient loadBalancerClient;
+
+    @GetMapping("consumer")
+    public Object consumer_dc(){
+
+        ServiceInstance serviceInstance = loadBalancerClient.choose("eureka-client");
+        String url = "http://"+ serviceInstance.getHost()+":"+serviceInstance.getPort()+"/dc";
+        System.out.println(url);
+        return restTemplate.getForObject(url, String.class);
+    }
+}
