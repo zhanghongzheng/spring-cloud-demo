3089fb80badc213e13a2120a1a4b25e2386687cf
zhz <783633439@qq.com>
2018-08-14 19:07:12 +0800

    eureka-server

---
+++ demo-eureka-server/src/main/java/com/zhz/demo/Application.java
@@ -0,0 +1,20 @@
+package com.zhz.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 功能:
+ *
+ * @author zhz
+ * @date 18/8/14 上午12:10
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class)
+                .web(true).run(args);
+    }
+}
