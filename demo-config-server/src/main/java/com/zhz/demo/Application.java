c6352242a99d9548b4f1b18dc3a1567b9ce232a9
zhz <783633439@qq.com>
2018-08-14 19:15:53 +0800

    config-server-git

---
+++ demo-config-server/src/main/java/com/zhz/demo/Application.java
@@ -0,0 +1,19 @@
+package com.zhz.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 功能:
+ *
+ * @author zhz
+ * @date 18/8/14 上午1:23
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+}
